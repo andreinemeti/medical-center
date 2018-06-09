b989784f0c0c7d6637e277a5b53cfcdedb79f335
Andrei Nemeti <nemetimarian@yahoo.com>
2018-06-10 01:49:40 +0300

    changes

--- js/main.js
+++ js/main.js
@@ -1,3 +1,16 @@
+//wow.js on scroll animations initialization
+  wow = new WOW(
+      {
+      animateClass: 'animated',
+      mobile: false,
+      offset: 50
+    }
+  );
+  wow.init();
+
+
+
+
 // home slider
   $('.home-slider').owlCarousel({
     loop:true,
@@ -24,3 +37,36 @@
       }
     }
   });
+
+
+  // owl carousel
+  var teamCarousel = $('.teamCarousel');
+  teamCarousel.owlCarousel({
+    loop:true,
+    autoplay: true,
+    stagePadding: 7,
+    margin: 20,
+    animateOut: 'fadeOut',
+    animateIn: 'fadeIn',
+    nav: true,
+    autoplayHoverPause: true,
+    items: 3,
+    navText : ["<span class='ion-chevron-left'></span>","<span class='ion-chevron-right'></span>"],
+    responsive:{
+      0:{
+        items:1,
+        nav:false
+      },
+      600:{
+        items:2,
+        nav:false
+      },
+      1000:{
+        items:3,
+        nav:true,
+        loop:false
+      }
+    }
+  });
+
+
