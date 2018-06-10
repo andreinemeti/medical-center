389a35056c0cc3209bd12391e724877cd6a077b7
Andrei Nemeti <nemetimarian@yahoo.com>
2018-06-10 18:03:47 +0300

    added new sections

--- js/main.js
+++ js/main.js
@@ -54,35 +54,6 @@
   });
 
 
-  // owl carousel
-  var teamCarousel = $('.teamCarousel');
-  teamCarousel.owlCarousel({
-    loop:true,
-    autoplay: true,
-    stagePadding: 7,
-    margin: 20,
-    animateOut: 'fadeOut',
-    animateIn: 'fadeIn',
-    nav: true,
-    autoplayHoverPause: true,
-    items: 3,
-    navText : ["<span class='ion-chevron-left'></span>","<span class='ion-chevron-right'></span>"],
-    responsive:{
-      0:{
-        items:1,
-        nav:false
-      },
-      600:{
-        items:2,
-        nav:false
-      },
-      1000:{
-        items:3,
-        nav:true,
-        loop:false
-      }
-    }
-  });
 
 
 
